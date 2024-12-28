55c3542198126608f77cef24135eccd00f088c36
B-priyam <priyam3801h@gmail.com>
2024-12-28 19:20:58 +0530

    final commit

--- lib/globalApi.ts
+++ lib/globalApi.ts
@@ -3,7 +3,7 @@ import { resumeType } from "./types";
 
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:1337/api/",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL+ "/api/",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_KEY}`,
